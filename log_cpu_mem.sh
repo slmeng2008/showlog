1ad85c13166e0028351ad922a57f91ffb47effd5
shlmeng <shlmeng@grandstream.cn>
2020-09-09 16:06:02 +0800

    修正cpu使用率一直固定的问题

--- log_cpu_mem.sh
+++ log_cpu_mem.sh
@@ -5,7 +5,7 @@ while true
  do
     #vm_mem=$(free -m|grep "buffers/cache"|awk '{print $4}')
     vm_mem=$(free -m|grep "Mem"|awk '{print $7}')
-    cpu=$(top -bn1|grep "Cpu(s)"|awk '{print $8}'|awk -F'%' '{print $1}'|tail -n1)
+    cpu=$(top -bn2|grep "Cpu(s)"|awk '{print $8}'|awk -F'%' '{print $1}'|tail -n1)
     echo "mem $vm_mem" >> $logfile
     echo "cpu $cpu" >> $logfile
     sleep 1m
